7ccf1b5d81eca86c24cf324e791449a53e4164ec
shoma-mano <ms2geki@gmail.com>
2024-09-05 16:09:17 +0900

    improve: export type

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import type { ECSTaskDefinition } from "./interface";
 import { writeFileSync } from "fs";
 import getCallerFile from "get-caller-file";
+
 export const defineTaskDefinition = (
   props: ECSTaskDefinition,
   distPath?: string,
 ) => {
+  console.log("defineTaskDefinition");
   const callerFile = getCallerFile();
   if (!callerFile) {
     throw new Error("Cannot find caller file");
   }
   distPath = distPath ?? (callerFile.replace(".ts", ".json") as string);
+  console.log(`Writing to ${distPath}`);
   const json = JSON.stringify(props, null, 2);
   writeFileSync(distPath, json);
 };
+
+export type { ECSTaskDefinition } from "./interface";
