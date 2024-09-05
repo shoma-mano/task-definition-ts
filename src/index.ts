e4cea4eb6044f807cbc54a94e4ea075461f6cab7
shoma-mano <ms2geki@gmail.com>
2024-09-05 15:49:36 +0900

    fix: fix output path

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-import { basename, join } from "path";
 import type { ECSTaskDefinition } from "./interface";
 import { writeFileSync } from "fs";
-
+import getCallerFile from "get-caller-file";
 export const defineTaskDefinition = (
   props: ECSTaskDefinition,
   distPath?: string,
 ) => {
-  const fileName = basename(__filename).replace(".ts", ".json");
-  distPath = distPath ?? join(__dirname, fileName);
+  const callerFile = getCallerFile();
+  if (!callerFile) {
+    throw new Error("Cannot find caller file");
+  }
+  distPath = distPath ?? (callerFile.replace(".ts", ".json") as string);
   const json = JSON.stringify(props, null, 2);
   writeFileSync(distPath, json);
 };
