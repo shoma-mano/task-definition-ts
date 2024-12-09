b87fa2a43a9ef1792fa1385a2b547a904b66fec2
shoma-mano <ms2geki@gmail.com>
2024-12-09 19:39:34 +0900

    fix: change getCallerFile to caller-path

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import type { ECSTaskDefinition } from "./interface";
 import { writeFileSync } from "fs";
-import getCallerFile from "get-caller-file";
+import callerPath from "caller-path";
 
 export const defineTaskDefinition = (
   props: ECSTaskDefinition,
   distPath?: string,
 ) => {
   console.log("defineTaskDefinition");
-  const callerFile = getCallerFile();
+  const callerFile = callerPath();
   if (!callerFile) {
     throw new Error("Cannot find caller file");
   }
